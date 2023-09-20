6d7add2e4357ef47be3d3c4268a65d42d79a7401
Joao Claudio <pansanijoaomendonca@gmail.com>
2023-09-20 18:25:52 -0300

    fix issues

--- scripts/script.js
+++ scripts/script.js
@@ -343,3 +343,12 @@ document.addEventListener("keydown", (event) => {
     closeByOverlay();
   }
 });
+
+enableValidation({
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+});
